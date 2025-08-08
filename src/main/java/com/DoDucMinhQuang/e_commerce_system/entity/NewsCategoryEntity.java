240789d8c11903ae849b5f3a131b7feabed59f1e
Quang <quangquan23451822@gmail.com>
2025-08-08 17:14:13 +0700

    feat: create News-Category entity and dto

---
+++ src/main/java/com/DoDucMinhQuang/e_commerce_system/entity/NewsCategoryEntity.java
@@ -0,0 +1,68 @@
+package com.DoDucMinhQuang.e_commerce_system.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "NEWS_CATEGORY")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Getter
+@Setter
+public class NewsCategoryEntity {
+
+    @Id
+    @Column(name = "ID")
+    Long id;
+
+    @Column(name = "NAME", nullable = false, columnDefinition = "NVARCHAR(500)")
+    String name;
+
+    @Column(name = "NOTES", columnDefinition = "NTEXT")
+    String notes;
+
+    @Column(name = "ICON", columnDefinition = "NVARCHAR(250)")
+    String icon;
+
+    @Column(name = "IDPARENT")
+    Long idParent;
+
+    @Column(name = "SLUG", columnDefinition = "NVARCHAR(160)")
+    String slug;
+
+    @Column(name = "META_TITLE", columnDefinition = "NVARCHAR(100)")
+    String metaTitle;
+
+    @Column(name = "META_KEYWORD", columnDefinition = "NVARCHAR(500)")
+    String metaKeyword;
+
+    @Column(name = "META_DESCRIPTION", columnDefinition = "NVARCHAR(500)")
+    String metaDescription;
+
+    @Column(name = "CREATED_DATE", columnDefinition = "DATETIME DEFAULT GETDATE()")
+    LocalDateTime createdDate;
+
+    @Column(name = "UPDATED_DATE", columnDefinition = "DATETIME DEFAULT GETDATE()")
+    LocalDateTime updatedDate;
+
+    @Column(name = "CREATED_BY")
+    Long createdBy;
+
+    @Column(name = "UPDATED_BY")
+    Long updatedBy;
+
+    @Column(name = "ISDELETE", columnDefinition = "TINYINT DEFAULT 0")
+    Byte isDelete;
+
+    @Column(name = "ISACTIVE", columnDefinition = "TINYINT DEFAULT 1")
+    Byte isActive;
+}
