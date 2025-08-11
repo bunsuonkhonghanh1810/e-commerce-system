cedeb6df268536e4bef925d9105abeee70cd1120
Quang <quangquan23451822@gmail.com>
2025-08-11 08:54:05 +0700

    feat: add NewsCategoryRepository for database access

---
+++ src/main/java/com/DoDucMinhQuang/e_commerce_system/repository/NewsCategoryRepository.java
@@ -0,0 +1,7 @@
+package com.DoDucMinhQuang.e_commerce_system.repository;
+
+import com.DoDucMinhQuang.e_commerce_system.entity.NewsCategoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NewsCategoryRepository extends JpaRepository<NewsCategoryEntity, Long> {
+}
