e0495c9f442f1a28df3d8b81d8dace06ec802f07
Quang <quangquan23451822@gmail.com>
2025-08-11 09:28:00 +0700

    feat: add ProductImagesRepository for database access

---
+++ src/main/java/com/DoDucMinhQuang/e_commerce_system/repository/ProductImagesRepository.java
@@ -0,0 +1,7 @@
+package com.DoDucMinhQuang.e_commerce_system.repository;
+
+import com.DoDucMinhQuang.e_commerce_system.entity.ProductImagesEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductImagesRepository extends JpaRepository<ProductImagesEntity, Long> {
+}
