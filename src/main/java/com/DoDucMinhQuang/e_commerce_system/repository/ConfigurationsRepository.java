2849e53ad26f9cc05d43d6b225f67c03077bb75f
Quang <quangquan23451822@gmail.com>
2025-08-11 08:51:17 +0700

    feat: add ConfigurationsRepository for database access

---
+++ src/main/java/com/DoDucMinhQuang/e_commerce_system/repository/ConfigurationsRepository.java
@@ -0,0 +1,7 @@
+package com.DoDucMinhQuang.e_commerce_system.repository;
+
+import com.DoDucMinhQuang.e_commerce_system.entity.ConfigurationsEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConfigurationsRepository extends JpaRepository<ConfigurationsEntity, Long> {
+}
