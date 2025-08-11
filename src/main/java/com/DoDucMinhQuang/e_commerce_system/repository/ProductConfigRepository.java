02f2bc2986b2c6bf213b18ed4d85be28f4bc12e3
Quang <quangquan23451822@gmail.com>
2025-08-11 09:02:39 +0700

    feat: add ProductConfigRepository for database access

---
+++ src/main/java/com/DoDucMinhQuang/e_commerce_system/repository/ProductConfigRepository.java
@@ -0,0 +1,7 @@
+package com.DoDucMinhQuang.e_commerce_system.repository;
+
+import com.DoDucMinhQuang.e_commerce_system.entity.ProductConfigEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductConfigRepository extends JpaRepository<ProductConfigEntity, Long> {
+}
